0ec0f378b7f49ead572ef6a9e39136a86465cae4
ljx <jasonlijiaxiang@gmail.com>
2017-05-04 15:05:21 +0800

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("this is my testing files");
+
+        System.out.println("this is a test file");
+    }
+}
